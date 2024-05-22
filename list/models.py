9024c0b003fee4837512c98a0e93b3fcfb1f5396
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-05-22 10:25:16 +0200

    created models.py

--- list/models.py
+++ list/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    tag = models.ManyToManyField(Tag, blank=True, null=True, related_name="tags")
+
+    class Meta:
+        ordering = ["completed", "-created_at"]
+
+    def __str__(self):
+        return self.content
