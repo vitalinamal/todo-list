4178ff453799829d41656d1933d00a8a8c8323c2
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-05-22 11:07:13 +0200

    added models to admin page

--- list/admin.py
+++ list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from list.models import Task, Tag
 
-# Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Task)
+admin.site.unregister(Group)
