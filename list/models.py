0db56e36c1d85d5ce3c9c77c253896a6fe2b50a6
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-05-22 13:45:07 +0200

    updated deadline field from DateField to DateTimeField

--- list/models.py
+++ list/models.py
@@ -14,9 +14,9 @@ class Tag(models.Model):
 class Task(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateField(null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tag, blank=True, related_name="tags")
+    tags = models.ManyToManyField(Tag, blank=True, related_name="tasks")
 
     class Meta:
         ordering = ["completed", "-created_at"]
